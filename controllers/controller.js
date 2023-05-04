1595c81b69a5fe696680fdd87f99f0ec990f69fc
Mayank <mayankbansal125@gmail.com>
2023-05-04 19:46:26 +0530

    Get,Post,Delete route for question and result

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,22 +1,52 @@
+import Result from "../models/resultSchema.js";
+import Questions from "../models/questionSchema.js";
+import questions, {answers} from "../database/data.js";
 
 // For question routes
 export async function getQuestions(req,res){
-    res.json("Get Question request");
+    try {
+        const questions=await Questions.find();
+        res.json(questions);
+    } catch (error) {
+        res.json(error);
+    }
 }
 export async function postQuestions(req,res){
-    res.json("Post New Question request");
+    try {
+        Questions.insertMany({questions,answers}).then(()=>{
+            res.json("Successfully posted the question");
+        })  
+    } catch (error) {
+        res.json(error);
+    }
 }
 export async function dropQuestions(req,res){
-    res.json("Delete Question request");
+    try {
+        Questions.deleteMany().then(()=>{
+            res.json("Deleted Questions!");
+        })
+    } catch (error) {
+        res.json(error);
+    }
 }
 
 // For result routes
 export async function getResult(req,res){
-    res.json("Get Result request");
+    const results=await Result.find();
+    res.json(results);
 }
 export async function storeResult(req,res){
-    res.json("Store/Post New Result request");
+    const { username, result, attempts, points, achieved}=req.body;
+    Result.create({ username, result, attempts, points, achieved}).then(()=>{
+        if(!username && !result){
+            throw new Error("Data not found!");
+        }
+        res.json("Result stored successfully!");
+    }).catch(error=>{
+        res.json("Error in storing the result",error);
+    });
 }
 export async function dropResult(req,res){
-    res.json("Delete Result request");
+    await Result.deleteMany();
+    res.json("Result deleted successfully!");
 }
